6937c25a39e370fcaf56b222a4f92fdb3f09deeb
saasbook <saasbook@saasbook.(none)>
2014-06-16 07:42:20 -0300

    second scenario green

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -18,7 +18,7 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
     when /^the edit page for "(.+)"$/i
-	edit movie_path(Movie.find_by_title $1) 
+	edit_movie_path(Movie.find_by_title $1) 
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
